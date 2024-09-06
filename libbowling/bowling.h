f0fb655018bf6bd01ff478991e214ad7ce6de4aa
Alexandre Vaughan <alexandre.vaughan@hexagon.com>
2024-09-06 13:23:10 +0200

    brain stimulatory 2024-09-06

--- libbowling/bowling.h
+++ libbowling/bowling.h
@@ -1,2 +1,22 @@
 
 bool bowling();
+
+class Game
+{
+public:
+    bool roll(int pins){
+
+        if (pins == 10)
+        {
+            m_score = 2* pins;
+            return true;
+        }
+        m_score += pins;
+        return m_score <= 10;
+     }
+
+    int score() const {return m_score;}
+
+private:
+    int m_score = 0;
+};
--- test/testbowling/testbowling.cpp
+++ test/testbowling/testbowling.cpp
@@ -2,9 +2,37 @@
 
 #include "bowling.h"
 
+class BowlingTest : public ::testing::Test {
+protected:
+    Game m_game;
+};
 
-TEST(BowlingTest, bowlingOk) {
-    EXPECT_FALSE(bowling());
+
+TEST_F(BowlingTest, noPins) {
+    m_game.roll(0);
+    EXPECT_EQ(m_game.score(), 0);
 }
 
+TEST_F(BowlingTest, pinLimit) {
+    EXPECT_TRUE(m_game.roll(4));
+    EXPECT_EQ(m_game.score(), 4);
+    
+    EXPECT_FALSE(m_game.roll(7));
+}
+
+TEST_F(BowlingTest, validPins) {
+    EXPECT_EQ(m_game.score(), 0);
+    
+    m_game.roll(3);
+    EXPECT_EQ(m_game.score(), 3);
+
+    m_game.roll(4);
+    EXPECT_EQ(m_game.score(), 7);
+}
+
+TEST_F(BowlingTest, checkIfRollIsStrike)
+{
+    m_game.roll(10);
+    EXPECT_EQ(m_game.score(), 20);
+}
 
